d1c9ed3bef8f92dbe66305e1bba1c9c23740658a
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-06-01 03:55:39 +0600

    jQuery support added

--- gReetr.js
+++ gReetr.js
@@ -38,21 +38,21 @@
         fullName: function () {
             return this.firstName + " " + this.lastName;
         },
-
+        //check if chosen laguage is supported
         validLanguage: function () {
             if (supportedLanguages.indexOf(this.language) === -1) {
                 throw "Unsupported Language";
             }
         },
-
+        //for casul greetings
         greetMessageCasual: function () {
             return casualGreetings[this.language] + " " + this.firstName;
         },
-
+        //for formal greetings
         greetMessageFormal: function () {
             return formalGreetings[this.language] + ", " + this.fullName();
         },
-
+        //show the greet message
         greetDisplay: function (formal) {
             var message;
             if (formal) {
@@ -67,7 +67,7 @@
 
             return this;
         },
-
+        //show logged message
         logDisplay: function () {
             if (console) {
                 console.log(logMessages[this.language] + ", " + this.fullName());
@@ -75,11 +75,31 @@
 
             return this;
         },
-
+        //choose language on the fly
         setLanguage: function (lang) {
             this.language = lang;
 
             this.validLanguage();
+
+            return this;
+        },
+        //adding jQuery support
+        HTMLgreetings: function (selector, formal) {
+            if (!$) {
+                throw "jQuery not loaded";
+            }
+            if (!selector) {
+                throw "Missing jQuery HTML Selector";
+            }
+
+            var message;
+            if (formal) {
+                message = this.greetMessageFormal();
+            } else {
+                message = this.greetMessageCasual();
+            }
+
+            $(selector).html(message);
 
             return this;
         }
